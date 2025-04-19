b223d333869e7477f5c2476acac021f9aa92dfea
vipulmalik07 <chvipulmalik07@gmail.com>
2025-04-19 23:42:40 +0530

    Layout Ch.

--- src/data.js
+++ src/data.js
@@ -3,8 +3,8 @@ export const userData = {
     title : "Developer",
     location : "Dehradun, India",
     social: [
-        {name: "Github", url: "https://github.com/in/Workwithshi", icon: "/public/images/github.png"},
-        {name: "LinkedIn", url: "https://linkedin.com/in/Himanshi404", icon: "/public/images/linkedin.png"},
-        {name: "X", url: "https://x.com/Ukiyoshiii", icon:"/public/images/x.png" }
+        {name: "Github", url: "https://github.com/in/Workwithshi", icon: "/images/github.png"},
+        {name: "LinkedIn", url: "https://linkedin.com/in/Himanshi404", icon: "/images/linkedin.png"},
+        {name: "X", url: "https://x.com/Ukiyoshiii", icon:"/images/x.png" }
     ]
 }
